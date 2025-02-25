a125d03d197bdb6a9dd116a3f457eb61ce19faab
ismailnajah <for1ismail@gmail.com>
2025-02-25 11:59:19 +0000

    added builtin cd and error checks

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <stdbool.h>
+#include <errno.h>
 
 typedef struct pipeline
 {
@@ -21,6 +22,27 @@ typedef struct command
 	int		exit_status;
 } t_command;
 
+size_t	ft_strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+bool	ft_error(char *error, char *arg)
+{
+	char *prefix = "error: ";
+
+	write(STDERR_FILENO, prefix, ft_strlen(prefix));
+	write(STDERR_FILENO, error, ft_strlen(error));
+	write(STDERR_FILENO, " ", 1);
+	write(STDERR_FILENO, arg, ft_strlen(arg));
+	write(STDERR_FILENO, "\n", 1);
+	return (false);
+}
+
 t_pipeline get_next_pipeline(int ac, char **av, int i)
 {
 	t_pipeline pipeline;
@@ -34,18 +56,6 @@ t_pipeline get_next_pipeline(int ac, char **av, int i)
 	}
 	pipeline.end = i;
 	return (pipeline);
-}
-
-void	print(char **av, int start, int end)
-{
-	int i = start;
-	printf("DEBUG :{ ");
-	while (i < end)
-	{
-		printf("%s ", av[i]);
-		i++;
-	}
-	printf("}\n");
 }
 
 t_command	get_next_command(char **av, int start, int end)
@@ -71,8 +81,13 @@ bool	dup2_and_close(int pipe_fd, int std_fd)
 {
 	if (pipe_fd != std_fd)
 	{
-		dup2(pipe_fd, std_fd);
-		close(pipe_fd);
+		if (dup2(pipe_fd, std_fd) == -1)
+		{
+			printf("file descriptor: %d %d\n", pipe_fd, std_fd);
+			return ft_error("fatal", strerror(errno));
+		}
+		if (close(pipe_fd) == -1)
+			return ft_error("fatal", "");
 	}
 	return (true);
 }
@@ -85,7 +100,7 @@ void	child_process(t_command *cmd, char **av, char **env)
 		exit(1);
 	av[cmd->end] = NULL;
 	if (execve(av[cmd->start], av + cmd->start, env) != 0)
-		exit(2);
+		exit(ft_error("cannot execute", av[cmd->start]));
 }
 
 #define RD 0
@@ -99,30 +114,42 @@ bool	fork_and_execute(t_command *cmd, char **av, char **env, bool last)
 	if (!last)
 	{
 		if(pipe(pipe_fds) == -1)
-			return (false);
+			return ft_error("fatal", "");
 		cmd->out_fd = pipe_fds[WR];
 	}
 	cmd->pid = fork();
 	if (cmd->pid == -1)
-		return (false);
+		return ft_error("fatal", "");
 	if (cmd->pid == 0)
 	{
+		if (!last && close(pipe_fds[RD]) == -1)
+			exit(ft_error("fatal", ""));
 		cmd->in_fd = old_fd;
 		child_process(cmd, av, env);
 	}
 	else
 	{
-		if (old_fd != STDIN_FILENO)
-			close(old_fd);
+		if (old_fd != STDIN_FILENO && close(old_fd) == -1)
+			return ft_error("fatal", "");
 		if (!last)
 		{
-			close(pipe_fds[WR]);
+			if (close(pipe_fds[WR]) == -1)
+				return ft_error("fatal", "");
 			old_fd = pipe_fds[RD];
 		}
 		else
 			old_fd = STDIN_FILENO;
 	}
 	return (true);
+}
+
+int	builtin_cd(t_pipeline pipeline, char **av)
+{
+	if (pipeline.end - pipeline.start != 2)
+		return ft_error("cd: bad arguments", "");
+	if(chdir(av[pipeline.start + 1]) == -1)
+		return ft_error("cd: cannot change directory to", av[pipeline.start + 1]);
+	return (0);
 }
 
 int	run_pipeline(t_pipeline pipeline, char **av, char **env)
@@ -132,11 +159,14 @@ int	run_pipeline(t_pipeline pipeline, char **av, char **env)
 	t_command cmd;
 
 	i = pipeline.start;
+	if (strcmp(av[i], "cd") == 0)
+		return (builtin_cd(pipeline, av));
 	while (i < pipeline.end)
 	{
 		cmd = get_next_command(av, i, pipeline.end);
 		last = (cmd.end == pipeline.end);
-		fork_and_execute(&cmd, av, env, last);
+		if(!fork_and_execute(&cmd, av, env, last))
+			exit(3);
 		i = cmd.end + 1;
 	}
 	waitpid(cmd.pid, &cmd.exit_status, 0);
@@ -153,7 +183,6 @@ int main(int ac, char **av, char **env)
 	{
 		pipeline = get_next_pipeline(ac, av, i);
 		ret = run_pipeline(pipeline, av, env);
-		//print_pipeline(pipeline, av);
 		i = pipeline.end + 1;
 	}
 	return (ret);
